8e200506ac6157b72c708a99769198817e0faad1
Franco <frandeluca22@gmail.com>
2020-08-21 20:19:58 -0500

    Add module date.js, add work list, add header + footer with EJS

--- app.js
+++ app.js
@@ -1,34 +1,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-var items = [];
-
 app.set("view engine", "ejs");
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let workItems = [];
+let items = [];
+
 app.get("/", function (req, res) {
-  var today = new Date();
+  let day = date.date();
+  res.render("list", { listTitle: day, newListItems: items });
+});
 
-  var options = {
-      weekday: "long",
-      day: "numeric", 
-      month: "long"
-  };
-
-  var day = today.toLocaleDateString("en-US", options);
-
-  res.render("list", { kindOfDay: day, newListItems: items });
+app.get("/work", function(req, res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
 
 app.post("/", function(req, res){
-    var item = req.body.newItem;
-    items.push(item);
+    let item = req.body.newItem;
 
-    res.redirect("/");
+    console.log(req.body);
+
+    if (req.body.list === "Work") {
+      workItems.push(item);
+      res.redirect("/work");
+    } else {
+      items.push(item);
+      res.redirect("/");
+    };
+    
 });
 
 app.listen(3000, function () {
